a762a438fc4533761fae225808baf429aa649de9
Stephen <profech.itservices@gmail.com>
2023-07-05 12:48:30 +0100

    Completed landing page design with two functionals, collecting long link and displayin shortnened link

--- slicr/src/App.jsx
+++ slicr/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage';
+import Auth from './Components/Auth/Auth';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element = {<LandingPage />} />
+        {/* <Route path='/' element = {<Auth />} /> */}
+        <Route path='/' element = {<LandingPage/>} />
+        <Route path='/dashboard' element = {<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
--- slicr/src/Components/LandingPage/LandingPage.jsx
+++ slicr/src/Components/LandingPage/LandingPage.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {Container, Row, Col, Form} from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <>
+      <Container >
+        <Row>
+          <Col>
+          <Card>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+          <Form.Label htmlFor="basic-url">Slicr URL Shortner</Form.Label>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon3">
+           Enter URL to shorten
+          </InputGroup.Text>
+          <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+        </InputGroup>
+          </ListGroup.Item>
+          <ListGroup.Item>Shortened URL : </ListGroup.Item>
+        </ListGroup>
+          </Card>
+          </Col>
+
+        </Row>
+
+      </Container>
+       
+    </>
   )
 }
